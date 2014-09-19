6cd2a5eca5d658de1dad3619ae9b70cb31172cf2
tchodey <tchodey@cisco.com>
2014-09-19 15:00:21 -0400

    initial commit

---
+++ app/src/main/java/dft/cisco/com/sampleapplication/MyActivity.java
@@ -0,0 +1,45 @@
+package dft.cisco.com.sampleapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.cisco.dft.mymodule.LibActivity1;
+
+
+public class MyActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.my, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void onButton1Clicked(View view) {
+        Intent intent = new Intent(this, LibActivity1.class);
+        startActivity(intent);
+    }
+}
